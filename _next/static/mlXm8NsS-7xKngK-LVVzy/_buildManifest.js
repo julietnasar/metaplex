f70d3c0481c776e096067e39cded1d507ce341dd
Juliet Nasar <73414329+julietnasar@users.noreply.github.com>
2022-02-14 15:22:04 -0500

    Updates

---
+++ _next/static/mlXm8NsS-7xKngK-LVVzy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-728972a14f3aab16b02a.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
